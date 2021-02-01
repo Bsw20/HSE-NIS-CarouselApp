a5a51da62f618229297be20ebfbad413dcf0248e
Bsw20 <yaakarpunkin@edu.hse.ru>
2021-02-01 21:13:25 +0300

    last commit

--- HSE-NIS-CarouselApp/Controllers/TripViewController.swift
+++ HSE-NIS-CarouselApp/Controllers/TripViewController.swift
@@ -9,20 +9,40 @@ import UIKit
 import SnapKit
 
 class TripViewController: UIViewController {
-
+    
+    private var trips =
+        [
+            Trip(tripId: "Paris001", city: "Paris", country: "France",
+    featuredImage: UIImage(named: "paris-2017-home"), price: 2000, totalDays: 5, isLiked:
+    false),
+            Trip(tripId: "Rome001", city: "Rome", country: "Italy", featuredImage:UIImage(named: "italy"), price: 800, totalDays: 3, isLiked: false),
+            Trip(tripId: "Istanbul001", city: "Istanbul", country: "Turkey",
+    featuredImage: UIImage(named: "stambul-dostoprimechatelnosti"), price: 2200, totalDays: 10, isLiked:true),
+        ]
     //MARK: - Controls
     private var collectionView: UICollectionView!
-    private lazy var label: UILabel = {
+    private lazy var topLabel: UILabel = {
         let label = UILabel()
-        label.text = "Most Popular Destinations"
+        label.text = "Most Popular \nDestinations"
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.numberOfLines = 2
+        return label
+    }()
+    private lazy var bottomLabel: UILabel = {
+        let label = UILabel()
+        label.text = "APPCODA"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.numberOfLines = 2
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .lightGray
         setupCollection()
+        setupConstraints()
     }
     
     //MARK: - funcs
@@ -32,9 +52,70 @@ class TripViewController: UIViewController {
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.showsVerticalScrollIndicator = false
         collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: CustomCell.reuseId)
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
     }
+}
 
+//MARK: - TripCollectionCellDelegate
+extension TripViewController: TripCollectionCellDelegate {
+    func didLikeButtonPressed(cell: UICollectionViewCell) {
+        if let indexPath = collectionView.indexPath(for: cell) {
+            trips[indexPath.row].isLiked = !trips[indexPath.row].isLiked
+            collectionView.reloadData()
+        }
+    }
+    
+    
+}
 
+//MARK: - CollectionView
+extension TripViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return trips.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCell.reuseId, for: indexPath) as! CustomCell
+        cell.set(trip: trips[indexPath.item])
+        cell.delegate = self
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenSize = UIScreen.main.bounds
+        return  CGSize(width: screenSize.width * 0.6, height: screenSize.height * 0.5)
+    }
+    
+    
+}
+
+//MARK: - Constraints
+extension TripViewController {
+    private func setupConstraints() {
+        view.addSubview(collectionView)
+        view.addSubview(topLabel)
+        view.addSubview(bottomLabel)
+        
+        collectionView.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.5)
+            make.centerY.equalToSuperview()
+        }
+        
+        topLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.equalToSuperview().offset(20)
+        }
+        
+        bottomLabel.snp.makeConstraints { (make) in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.left.equalToSuperview().offset(20)
+        }
+    }
 }
 
 // MARK: - SwiftUI
---
+++ HSE-NIS-CarouselApp/Views/CustomCell.swift
@@ -0,0 +1,173 @@
+//
+//  CustomCell.swift
+//  HSE-NIS-CarouselApp
+//
+//  Created by Ярослав Карпунькин on 01.02.2021.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+protocol TripCollectionCellDelegate: NSObjectProtocol {
+    func didLikeButtonPressed(cell: UICollectionViewCell)
+}
+
+class CustomCell: UICollectionViewCell {
+    //MARK: - Variables
+    public static var reuseId = "CustomCell"
+    var isLiked: Bool = false {
+        didSet {
+            if isLiked {
+                likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            } else {
+                likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            }
+        }
+    }
+    
+    weak var delegate: TripCollectionCellDelegate?
+    
+    //MARK: - Controls
+    private lazy var containerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .gray
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var bottomView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    
+    private lazy var cityLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.text = "City"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var countryLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.text = "country"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var daysLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.text = "5 days"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.text = "2000$"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        return label
+    }()
+    
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        button.tintColor = .red
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+        likeButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    @objc private func likeButtonTapped() {
+        delegate?.didLikeButtonPressed(cell: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        print(#function)
+        likeButton.imageView?.contentMode = .scaleAspectFill
+        layer.cornerRadius = 4
+        clipsToBounds = true
+    }
+    
+    public func set(trip: Trip) {
+        cityLabel.text = trip.city
+        countryLabel.text = trip.country
+        imageView.image = trip.featuredImage
+        priceLabel.text = "\(String(trip.price))$"
+        daysLabel.text = "\(String(trip.totalDays))days"
+        isLiked = trip.isLiked
+    }
+}
+
+//MARK: - Constraints
+extension CustomCell {
+    private func setupConstraints(){
+        addSubview(containerView)
+        containerView.addSubview(bottomView)
+        containerView.addSubview(imageView)
+        
+        containerView.snp.makeConstraints { (make) in
+            make.left.bottom.top.right.equalToSuperview()
+        }
+        
+        bottomView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.19)
+        }
+        
+        imageView.snp.makeConstraints { (make) in
+            make.top.left.right.equalToSuperview()
+            make.bottom.equalTo(bottomView.snp.top)
+        }
+        
+        let stackView = UIStackView(arrangedSubviews: [cityLabel, countryLabel, daysLabel, priceLabel], axis: .vertical, spacing: 10)
+        
+        imageView.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { (make) in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        bottomView.addSubview(likeButton)
+        
+        likeButton.snp.makeConstraints { (make) in
+            make.centerY.centerX.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.7)
+            make.width.equalTo(likeButton.snp.height)
+        }
+        
+        likeButton.imageView?.snp.makeConstraints({ (make) in
+            make.width.height.equalToSuperview()
+        })
+    }
+}
